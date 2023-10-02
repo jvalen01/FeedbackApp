06751b7c6e8209fb5d0260e1e2ceea902b594305
jvalen01 <villafifa@outlook.com>
2023-10-02 12:46:12 +0200

    initialize spring boot app

---
+++ src/test/java/dat250/feedApp/FeedAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dat250.feedApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
